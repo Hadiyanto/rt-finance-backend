00f3001693a084d313736abe6e3487e770708162
Hadiyanto <hadiyanto.hanasil@allofresh.id>
2025-12-13 12:17:27 +0700

    Fix cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ["http://localhost:3001", "http://192.168.18.52:3001", "https://rt-finance-frontend.vercel.app/"],
+  origin: ["http://localhost:3001", "http://192.168.18.52:3001", "https://rt-finance-frontend.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
