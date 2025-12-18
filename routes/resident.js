0b882a432399fa484ec9f2980bf19eb52ee09051
Hadiyanto <hadiyanto.hanasil@allofresh.id>
2025-12-18 19:16:35 +0700

    add get block and number

--- routes/resident.js
+++ routes/resident.js
@@ -7,26 +7,53 @@ const redis = require("../lib/redisClient");
 const auth = require("../middlewares/auth");
 
 
-router.get("/residents", auth(["admin", "bendahara"]), cacheResidents, async (req, res) => {
+router.get("/residents", auth(["admin", "bendahara"]), async (req, res) => {
   try {
-    const residents = await prisma.resident.findMany({
-      orderBy: { id: "asc" }
+    const {
+      page = 1,
+      limit = 50,
+      block,
+      search
+    } = req.query;
+
+    const skip = (page - 1) * limit;
+
+    const where = {};
+
+    // Filter by block
+    if (block) {
+      where.block = block;
+    }
+
+    // Search by name
+    if (search) {
+      where.fullName = {
+        contains: search,
+        mode: "insensitive"
+      };
+    }
+
+    const [data, total] = await Promise.all([
+      prisma.resident.findMany({
+        where,
+        skip: Number(skip),
+        take: Number(limit),
+        orderBy: { block: "asc" }
+      }),
+      prisma.resident.count({ where })
+    ]);
+
+    res.json({
+      page: Number(page),
+      limit: Number(limit),
+      total,
+      totalPages: Math.ceil(total / limit),
+      data
     });
 
-    const response = {
-      page: 1,
-      limit: residents.length,
-      total: residents.length,
-      data: residents
-    };
-
-    await redis.set("residents", JSON.stringify(response));
-
-    res.json(response);
-
   } catch (err) {
-    console.error("Error fetching residents:", err);
-    res.status(500).json({ message: "Failed to fetch residents" });
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch residents list" });
   }
 });
 
@@ -52,7 +79,6 @@ router.get("/residents/:id", auth(["admin", "bendahara"]), async (req, res) => {
     return res.status(500).json({ error: "Failed to fetch resident" });
   }
 });
-
 
 // Get resident by block + houseNumber
 router.get("/residents/:block/:houseNumber", auth(["admin", "bendahara"]), async (req, res) => {
@@ -190,56 +216,46 @@ router.get("/residents/:block/:houseNumber", auth(["admin", "bendahara"]), async
   }
 });
 
-// Get list of residents (with pagination & filter)
-router.get("/residents", auth(["admin", "bendahara"]), async (req, res) => {
+router.get("/blocks", async (req, res) => {
   try {
-    const {
-      page = 1,
-      limit = 50,
-      block,
-      search
-    } = req.query;
+    const blocks = await prisma.resident.findMany({
+      distinct: ["block"],
+      select: { block: true },
+      orderBy: { block: "asc" }
+    });
 
-    const skip = (page - 1) * limit;
-
-    const where = {};
-
-    // Filter by block
-    if (block) {
-      where.block = block;
-    }
-
-    // Search by name
-    if (search) {
-      where.fullName = {
-        contains: search,
-        mode: "insensitive"
-      };
-    }
-
-    const [data, total] = await Promise.all([
-      prisma.resident.findMany({
-        where,
-        skip: Number(skip),
-        take: Number(limit),
-        orderBy: { block: "asc" }
-      }),
-      prisma.resident.count({ where })
-    ]);
+    const blockList = blocks.map(b => b.block);
 
     res.json({
-      page: Number(page),
-      limit: Number(limit),
-      total,
-      totalPages: Math.ceil(total / limit),
-      data
+      total: blockList.length,
+      blocks: blockList
     });
 
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Failed to fetch residents list" });
+    console.error("Error fetching residents:", err);
+    res.status(500).json({ message: "Failed to fetch residents" });
   }
 });
 
+router.get("/houses-number", async (req, res) => {
+  try {
+    const numbers = await prisma.resident.findMany({
+      distinct: ["houseNumber"],
+      select: { houseNumber: true },
+      orderBy: { id: "asc" }
+    });
+
+    const houseNumberList = numbers.map(b => b.houseNumber);
+
+    res.json({
+      total: houseNumberList.length,
+      blocks: houseNumberList
+    });
+
+  } catch (err) {
+    console.error("Error fetching residents:", err);
+    res.status(500).json({ message: "Failed to fetch residents" });
+  }
+});
 
 module.exports = router;
